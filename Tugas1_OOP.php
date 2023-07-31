a26a171b7a44f73628e3b0d7325a8dc7a73a28f3
Monkey D Kevin <88268565+kevinadisuryanugraha@users.noreply.github.com>
2023-07-31 11:53:13 +0700

    Update Tugas1_OOP.php

--- Tugas1_OOP.php
+++ Tugas1_OOP.php
@@ -1,10 +1,13 @@
 <?php
 
+// Parent class Lemari
 class Lemari {
-    protected $merk;
+    // Property dengan access modifier sesuai permintaan
+    public $merk;
     protected $warna;
-    protected $kapasitas;
+    private $kapasitas;
 
+    // Constructor
     public function __construct($merk, $warna, $kapasitas) {
         $this->merk = $merk;
         $this->warna = $warna;
@@ -12,79 +15,79 @@ class Lemari {
         echo "Lemari {$this->merk} telah dibuat.<br>";
     }
 
+    // Destructor
     public function __destruct() {
         echo "Lemari {$this->merk} telah dihancurkan.<br>";
     }
 
-    public function setMerk($merk) {
-        $this->merk = $merk;
-    }
-
-    public function getMerk() {
-        return $this->merk;
-    }
-
+    // Setter
     public function setWarna($warna) {
         $this->warna = $warna;
-    }
-
-    public function getWarna() {
-        return $this->warna;
     }
 
     public function setKapasitas($kapasitas) {
         $this->kapasitas = $kapasitas;
     }
 
+    // Getter
+    public function getWarna() {
+        return $this->warna;
+    }
+
     public function getKapasitas() {
         return $this->kapasitas;
     }
 
+    // Method
     public function getInfo() {
-        return "Lemari {$this->merk}, warna {$this->warna}, kapasitas {$this->kapasitas} kg.";
+        return "Lemari {$this->merk}, warna {$this->warna}, isi Pakaian";
     }
 }
 
+// Child class Baju yang mengextends Lemari
 class Baju extends Lemari {
+    // Property tambahan khusus untuk class Baju
     private $bahan;
 
+    // Constructor
     public function __construct($merk, $warna, $kapasitas, $bahan) {
         parent::__construct($merk, $warna, $kapasitas);
         $this->bahan = $bahan;
         echo "Lemari baju dari {$this->bahan} telah dibuat.<br>";
     }
 
+    // Destructor
     public function __destruct() {
         echo "Lemari baju dari {$this->bahan} telah dihancurkan.<br>";
         parent::__destruct();
     }
 
+    // Setter
     public function setBahan($bahan) {
         $this->bahan = $bahan;
     }
 
+    // Getter
     public function getBahan() {
         return $this->bahan;
     }
 
+    // Method override (polymorphism)
     public function getInfo() {
-        return "Lemari baju dari {$this->bahan}, merk {$this->merk}, warna {$this->warna}, kapasitas {$this->kapasitas} kg.";
+        return "Lemari baju dari {$this->bahan}, merk {$this->merk}, warna {$this->getWarna()}, isi Barang Pribadi";
     }
 }
 
-$lemari1 = new Lemari("IKEA", "putih", 100);
-$baju1 = new Baju("Olympus", "coklat", 50, "kayu");
+// Instansiasi object dari masing-masing class
+$lemari1 = new Lemari("IKEA", "putih", "10 kg");
+$baju1 = new Baju("Olympus", "coklat", "5 kg", "kayu");
 
-$lemari1->setMerk("Minimalist");
+// Set semua property dari objectnya
+$lemari1->merk = "Minimalist";
 $lemari1->setWarna("hitam");
-$lemari1->setKapasitas(200);
-$baju1->setMerk("Levi's");
-$baju1->setWarna("hitam");
-$baju1->setKapasitas(70);
+$baju1->merk = "Convers";
 $baju1->setBahan("besi");
 
+// Get semua property dari objectnya
 echo "Info Lemari 1: " . $lemari1->getInfo() . "<br>";
 echo "Info Baju 1: " . $baju1->getInfo() . "<br>";
-
-echo $lemari1->getMerk() . "<br>";
-echo $baju1->getBahan() . "<br>";
